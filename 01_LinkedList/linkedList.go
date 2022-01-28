63380eb9ad13a0f3c8d77b9d1b5738fd031f1ead
Pavel Osminin <osperevod@gmail.com>
2022-01-29 03:04:08 +0500

    Remove is fixed

--- 01_LinkedList/linkedList.go
+++ 01_LinkedList/linkedList.go
@@ -71,7 +71,57 @@ func (n LinkedList) printLinkedList() {
 
 // to do
 // FindAll (slice append)
+
+func (n LinkedList) FindAll(data int) []Node {
+	// Finds all nodes by the specified data and returns a slice of nodes
+	var nodes []Node
+	currentNode := n.head
+	for currentNode != nil {
+		if currentNode.data == data {
+			nodes = append(nodes, *currentNode)
+		}
+		currentNode = currentNode.next
+	}
+	return nodes
+}
+
 // Remove (bool)
+func (n *LinkedList) Remove(data int) bool {
+	if n.Find(data) == nil || n.head == nil {
+		return false
+	} else {
+		currentNode := n.head
+		var previousNode *Node
+		for currentNode != nil {
+			if currentNode.data == data {
+				if n.head.next == nil {
+					n.head = nil
+					n.tail = nil
+					return true
+				}
+				if n.head.data == currentNode.data {
+					n.head = n.head.next
+					return true
+				} else {
+					if currentNode.next == nil {
+						previousNode.next = nil
+						n.tail = previousNode
+						return true
+					} else {
+						previousNode.next = currentNode.next
+						return true
+					}
+				}
+
+			}
+			previousNode = currentNode
+			currentNode = currentNode.next
+		}
+		return true
+	}
+
+}
+
 // RemoveAll
 // InsertAfter
 
@@ -80,11 +130,21 @@ func main() {
 	testNode := Node{data: 10}
 	testNode2 := Node{data: 20}
 	testNode3 := Node{data: 30}
+	testNode4 := Node{data: 30}
 
 	testList.addInTail(&testNode)
 	testList.addInTail(&testNode2)
 	testList.addInTail(&testNode3)
+	testList.addInTail(&testNode4)
 
+	fmt.Println("initial list:")
+	testList.printLinkedList()
+	fmt.Println(testList.Count())
+	fmt.Println("FindAll result:")
+	fmt.Println(testList.FindAll(30))
+	fmt.Println("Remove '30' value")
+	fmt.Println(testList.Remove(30))
+	fmt.Println("list after remove:")
 	testList.printLinkedList()
 	fmt.Println(testList.Count())
 
@@ -93,4 +153,5 @@ func main() {
 	} else {
 		fmt.Println("Linked list is incorrect!")
 	}
+
 }
